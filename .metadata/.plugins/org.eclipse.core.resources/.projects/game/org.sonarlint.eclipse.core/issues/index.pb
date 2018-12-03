be74653081a82a83d6906b6e94c7994a5f75c1cb
Andreas Lorenz <andreas.lorenz.2013@outlook.com>
2018-12-03 21:21:27 +0100

    [setup] added lombok and main class

--- .metadata/.plugins/org.eclipse.core.resources/.projects/game/org.sonarlint.eclipse.core/issues/index.pb
+++ .metadata/.plugins/org.eclipse.core.resources/.projects/game/org.sonarlint.eclipse.core/issues/index.pb
@@ -1,3 +1,5 @@
 
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+\
+,src/main/java/com/lorenz/game/main/Main.java,f\c\fca391f1cb1912e605346e897caa8440ff9b90f1
---
+++ game/src/main/java/com/lorenz/game/main/Main.java
@@ -0,0 +1,12 @@
+package com.lorenz.game.main;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class Main {
+	
+	public static void main(String[] args) {
+		LOGGER.info("Hello");
+	}
+
+}
